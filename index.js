3ff468d40e49c517b7a7da6c2de3eefc1bcd01ba
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-01 20:33:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// Variables store data for later use
+let x=7;
+console.log(x);
+
+let y=12;
+console.log(y);
+
+let myVariableNameIsLong = "this";
+console.log(myVariableNameIsLong);
+
+let num1 = 7;
+num1= num1+2;
+console.log(num1); //9
+num1=num1-5;//4
+console.log(num1);
+num1=num1*2;
+num1=num1/1;
+console.log(num1);//8
+
+// Multiple Variables
+let MyVar1 = 5;
+let MyVar2 = 10;
+MyVar1 = MyVar2; //Assign value of MyVar2 to MyVar1
+MyVar2=6;
+console.log(`MyVar1:: ${MyVar1}`)
+console.log(`MyVar2:: ${MyVar2}`) //Print the values on screen
+
+// Shortcuts
+let var1= 12;
+var1= var1+1; //Long way of adding
+var1++; //++ means the variable plus 1
+
+let var2=12;
+var2 = var2 -1; //Long Way of subtracting
+var2--; //Shortcut to subtracting by using --
+
+let var3=12;
+var3=var3+5; //add 5
+var3 +=5; //add 5, but quicker
+
+let var4 = 12;
+var4 = var4 * 3; //long way of multiplying variables
+var4 *=3; //short way of doing the same thing: multiply by 3
